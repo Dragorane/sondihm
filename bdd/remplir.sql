e56a2e33e131f4336478b3a8669a22d7bbd56d41
Loic <lois.laisne@gmail.com>
2015-05-02 19:17:41 +0200

    deplacement des fichiers bdd dans le dossier bdd, ajout d'un fichier d'initialisation des tables Revenu, Etat_Civil et TypeChamps

---
+++ bdd/remplir.sql
@@ -0,0 +1,18 @@
+INSERT into Etat_Civil values (1,"Célibataire");
+INSERT into Etat_Civil values (2,"Marié(e)");
+INSERT into Etat_Civil values (3,"Veuf(ve)");
+
+INSERT into Revenu values (1,0.0,10000.0);
+INSERT into Revenu values (2,10000.0,25000.0);
+INSERT into Revenu values (3,25000.0,75000.0);
+INSERT into Revenu values (4,75000.0,150000.0);
+INSERT into Revenu values (5,150000.0,999999.0);
+
+INSERT into Societe values (1,"ISN - Institut de sondage");
+
+INSERT into Type_Champs values (1,"input");
+INSERT into Type_Champs values (2,"dateinput");
+INSERT into Type_Champs values (3,"spinbox");
+INSERT into Type_Champs values (4,"doublespinboxe");
+INSERT into Type_Champs values (5,"radiobox");
+INSERT into Type_Champs values (6,"checkbox");
