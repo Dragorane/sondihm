aca5cfcf2e4b57115e188ca68a33b4255d46b21a
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2015-05-03 16:17:00 +0200

    Initializing default db.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+		QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+		db.setDatabaseName("ihm.db");
+		if (db.open())
+			return a.exec();
+		return -1;
 }
